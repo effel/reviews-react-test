ece8a604efe6407ee80154e4eec64764810f8e64
t.salikayeva <t.salikayeva@dynatech.lv>
2017-08-17 14:25:20 +0300

    added styles

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,37 +1,32 @@
  
 import React from 'react';
 import dateFormat from 'dateformat';
+import ShowAverage from './ShowAverage.jsx';
 
 import './App.scss';
 
+function filterArray(arrToFilter, itemToCompare, prop) {
+ arrToFilter.filter(function(elem){
+    return elem[prop] === itemToCompare;
+ });
+}
+
+function pagination(startPos, endPos, arr) {
+   let sliced = arr.slice(startPos, endPos);
+   return sliced;
+}
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-       ratings: {},
        repo: []
     };
   }  
 
   componentDidMount() {
       var that = this;
-      fetch("http://localhost:3000/ratings")
-      .then(  
-        function(response) {  
-          response.json().then(function(data) {  
-            
-            that.setState({
-                ratings: data
-            });  
-          });  
-        }  
-      )  
-      .catch(function(err) {  
-        console.log('Fetch Error :-S', err);  
-      });
-
       fetch("http://localhost:3000/reviews")
       .then(  
         function(response) {  
@@ -51,50 +46,74 @@ class App extends React.Component {
 
   render() {
 
-      var doubles2 = this.state.repo.map(function(x) {
-        x.entryDate = new Date(x.entryDate);
-        x.travelDate = new Date(x.travelDate);   
+      // var doubles2 = this.state.repo.map(function(x) {
+      //   x.entryDate = new Date(x.entryDate);
+      //   x.travelDate = new Date(x.travelDate);   
 
-         return  x;
-      });
+      //    return  x;
+      // });
 
       var doubles3 = this.state.repo.map(function(x) {
          return  x.traveledWith;
       });
 
-      const sorted = doubles2.sort((date1, date2) => {
-        return date1.entryDate - date2.entryDate;
-      })
-        console.log(sorted)
+      // const sorted = doubles2.sort((date1, date2) => {
+      //   return date1.entryDate - date2.entryDate;
+      // })
+      //   console.log(sorted)
 
-      const sorted2 = doubles2.sort((date1, date2) => {
-        return date1.travelDate - date2.travelDate;
-      })
+      // const sorted2 = doubles2.sort((date1, date2) => {
+      //   return date1.travelDate - date2.travelDate;
+      // })
 
-      var doubles2 = this.state.repo.map(function(x) {
-        x.entryDate = (dateFormat(x.entryDate,"dd/mm/yyyy")).toString();
-        x.travelDate = (dateFormat(x.travelDate,"dd/mm/yyyy")).toString();
-         return  x;
-      });  
+      // var doubles2 = this.state.repo.map(function(x) {
+      //   x.entryDate = (dateFormat(x.entryDate,"dd/mm/yyyy")).toString();
+      //   x.travelDate = (dateFormat(x.travelDate,"dd/mm/yyyy")).toString();
+      //    return  x;
+      // });  
 
       let unique = [...new Set(doubles3)]; 
-      console.log(unique)
 
-     const listItems = sorted2.map((number, index ) =>
+     let arr = pagination(0, 9, this.state.repo);
+
+     const listItems = arr.map((number, index ) =>
         <div key={index} className="row">
           <div  className="col-sm-12">
-            <p>  {number.travelDate}  </p>
+               <label className="text-primary"><small>Id: </small></label> <span>{number.id} </span>         
+               <label className="text-primary"><small>Travel with: </small></label> <span>{number.traveledWith} </span>
+               <label className="text-primary"><small>General rating: </small></label> <span>{number.ratings.general.general} </span>  
+               <label className="text-primary"><small>Travel date: </small></label> <span>{number.travelDate} </span>                 
+               <label className="text-primary"><small>Review date: </small></label> <span>{number.entryDate} </span>     
+               <hr />
           </div>
-        </div>        
-     );     
+        </div>   
+     );   
+
     return (
       <div className="container">
-        <div className="jumbotron text-center">
-          <h1>My Bootstrap Page</h1>
-          <p>{this.state.ratings.priceQuality}</p>
-        </div>      
+  
+        <ShowAverage />
+
+        <div className="row mb-4">
+          <div  className="col-sm-12">
+               <div className="dropdown">
+                  <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    Filter Travel With
+                    <span className="caret" />
+                  </button>
+                  <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" className="divider" />
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </div>
+          </div>
+        </div> 
 
         {listItems}
+
       </div>
     );
   }
@@ -102,3 +121,10 @@ class App extends React.Component {
 
 export default App;
 
+function getHtmlFromObj(obj) {
+  let teml = '';
+  for (var key in obj) {
+    teml += `<small><span class="text-info">Average of the ${key}:</span> ${obj[key]};</small> `;
+  }
+  return {__html: teml};
+}
---
+++ client/components/ShowAverage.jsx
@@ -0,0 +1,70 @@
+ 
+import React from 'react';
+
+function getHtmlFromObj(obj) {
+  let teml = '';
+  for (var key in obj) {
+    teml += `<small><span class="text-info">Average of the ${key}:</span> ${obj[key]};</small> `;
+  }
+  return {__html: teml};
+}
+
+class ShowAverage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       ratings: {}
+    };
+  }  
+
+  componentDidMount() {
+      var that = this;
+      fetch("http://localhost:3000/ratings")
+      .then(  
+        function(response) {  
+          response.json().then(function(data) {  
+            
+            that.setState({
+                ratings: data
+            });  
+          });  
+        }  
+      )  
+      .catch(function(err) {  
+        console.log('Fetch Error :-S', err);  
+      });
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <div className="jumbotron">
+
+          <div className="row">
+            <div  className="col-sm-12">
+                <h3 className="text-center mb-4"><label className="text-info">Average of the general rating of the accommodation:</label> {this.state.ratings.genRating}</h3>
+            </div>
+          </div>         
+          <div className="row mb-4">
+            <div  className="col-sm-12">          
+              <div dangerouslySetInnerHTML={getHtmlFromObj(this.state.ratings)} />   
+            </div>        
+          </div>
+          <div className="row">
+                <div  className="col-sm-2"><small><strong>Quantity of travellers with family:</strong> <span className="text-info">{this.state.ratings.travelWithFamily}</span> </small></div>
+                <div  className="col-sm-2"><small><strong>Quantity of travellers with friends:</strong> <span className="text-info">{this.state.ratings.travelWithFriends}</span> </small></div>                
+                <div  className="col-sm-2"><small><strong>Quantity of travellers with partner:</strong> <span className="text-info">{this.state.ratings.travelWithCouple}</span> </small></div>
+                <div  className="col-sm-2"><small><strong>Quantity of travellers as single:</strong> <span className="text-info">{this.state.ratings.travelWithSingle}</span> </small></div>
+                <div  className="col-sm-2"><small><strong>Quantity of travellers as other:</strong> <span className="text-info">{this.state.ratings.travelWithOthers}</span> </small></div>                
+          </div>            
+        </div>      
+
+      </div>
+    );
+  }
+}
+
+export default ShowAverage;
+
